a88ee687840f794c8350d63e7f4f1623f276ea5b
Nikhil22 <nikhil38@gmail.com>
2024-01-28 21:31:29 +0900

    update

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 This an experimental wing of [MangoCrisp](https://mangocrisp.com), a culinary genie in a bottle, giving you the ability to create unique and breathtaking dishes.
 
-![AI Generated Food Images](/collage.png)
-
+![AI Generated Food Images](images/collage.png)
 
 ## Stable Diffusion
 
 For stable diffusion experiments, check out my Notebooks in the [stable-diffusion](https://github.com/Nikhil22/mangocrisp-labs/tree/main/stable-diffusion) folder.
+
+## Effective Training
+
+For effective training experiments, check out my Notebooks in the [training](https://github.com/Nikhil22/mangocrisp-labs/tree/main/training) folder. This makes use of the [gym](https://github.com/Nikhil22/mangocrisp-labs/tree/main/gym module.
+
--- gym/util.py
+++ gym/util.py
@@ -2,6 +2,7 @@ import torch, random
 import numpy as np
 from operator import itemgetter
 from torch.utils.data import default_collate
+from .device import def_device
 
 def set_seed(seed, deterministic=False):
     torch.use_deterministic_algorithms(deterministic)
@@ -25,3 +26,22 @@ def inplace(f):
         f(b)
         return b
     return _f
+
+def conv(ni, nf, ks=3, stride=2, act=torch.nn.ReLU, norm=None, bias=None):
+    is_batch_norm = isinstance(norm, (
+        torch.nn.BatchNorm1d,
+        torch.nn.BatchNorm2d,
+        torch.nn.BatchNorm3d
+    ))
+    # the bias is redundant when doing batch normalization as this is captured by the 'shift'
+    if bias is None: bias = not is_batch_norm
+    layers = [torch.nn.Conv2d(ni, nf, stride=stride, kernel_size=ks, padding=ks//2, bias=bias)]
+    if norm: layers.append(norm(nf))
+    if act: layers.append(act())
+    return torch.nn.Sequential(*layers)
+
+def get_model(act=torch.nn.ReLU, nfs=None, norm=None):
+    if nfs is None: nfs = [1,8,16,32,64]
+    layers = [conv(nfs[i], nfs[i+1], act=act, norm=norm) for i in range(len(nfs)-1)]
+    return torch.nn.Sequential(*layers, conv(nfs[-1],10, act=None, norm=False, bias=True),
+                         torch.nn.Flatten()).to(def_device)
